d4f52b341674bbe9532bb3919b51c3a3287c57d6
Aarohi <aarohigupta2211@gmail.com>
2023-01-01 23:36:52 +0530

    fixing neural net

--- model-training/training_utils.py
+++ model-training/training_utils.py
@@ -9,39 +9,32 @@ from preprocessing_utils import get_train_test_dataloader
 
 # define a class for the neural network that inherits from nn.Module (base class for all neural network modules)
 class NeuralNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, class_size: int) -> None:
         """
         Define the layers of the neural network
         input: 1 x 28 x 28
-        output: 24 (since we have 24 classes)
+        output: 25 (since we have 25 classes)
         """
         super(NeuralNet, self).__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
-        # print the output shape of the convolutional layer
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(4, 4) # to reduce the size of the image to 32
-        self.fc1 = nn.Linear(512 * 7 * 7, 4096)
-        self.fc2 = nn.Linear(4096, 4096)
-        self.fc3 = nn.Linear(4096, 24)
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 6, 3)
+        self.conv3 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 48)
+        self.fc3 = nn.Linear(48, class_size)
 
     def forward(self, x):
         """
         Define the forward pass of the neural network
         """
-
-        x = torch.relu(self.conv1(x))
-        x = torch.relu(self.conv2(x))
-        x = torch.relu(self.conv3(x))
-        x = torch.relu(self.conv4(x))
-        x = torch.relu(self.conv5(x))
-        x = self.pool(x)
-        x = x.view(-1, 512 * 7 * 7)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
 
         return x
@@ -105,9 +98,6 @@ def train(model: nn.Module, train_loader: Dataset, criterion: nn.Module, optimiz
 
             # forward pass
             outputs = model(inputs)
-            # print(f"images: {inputs.size()}")
-            # print(f"labels: {labels.size()}")
-            # print(f"outputs: {outputs.size()}")
             loss = criterion(outputs, labels[:, 0])
 
             # backward pass + optimize
